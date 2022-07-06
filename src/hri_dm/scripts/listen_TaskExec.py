baba2e3ef17c0a82ab7244c8c3dcf88a05acd989
gpapo <grgepapadopoulos@gmail.com>
2022-07-06 17:42:28 +0300

    - fixing and renaming callbacks, added the two-HealthPost, fix import issues, simplify code, overall subscribe & post healthPost

--- src/hri_dm/scripts/listen_TaskExec.py
+++ src/hri_dm/scripts/listen_TaskExec.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-
-import requests
 import rospy
+import requests
+from datetime import datetime
 
-from std_msgs.msg import String
-from hri_dm.msg import HRIDM2TaskExecution, TaskExecution2HRIDM
-
+from std_msgs.msg import String, Float64
+from hri_dm.msg import HRIDM2TaskExecution, TaskExecution2HRIDM, Pose2D
+from forthHRIHealthPost import HRI_HealthStatePost
+HRI_health_jsonFName = r"/home/gpapo/Desktop/hri_ws/src/hri_dm/scripts/HRI_health.json"
+ScenePerception_health_jsonFName = r"/home/gpapo/Desktop/hri_ws/src/hri_dm/scripts/ScenePerception_health.json"
+address = "25.45.111.204"
+port = 1026
+my_date = datetime.utcnow()  # utc time, this is used in FELICE
 
 def callback1(data):
     print('______________________________________________callback____1')
@@ -23,20 +28,50 @@ def callback1(data):
         # newtask.action = 'newtask.action from else --> msg_from_callback1'
         # rospy.loginfo(newtask)
 
+def msge_decode_Pose2D(data):
+    global pub2Pose2D
+    pose_task = Pose2D()
+    pose_task.x = 1.1
+    pose_task.y = 2.2
+    pose_task.theta = 60.3
+    pose_task.timestamp = my_date.isoformat()
+    rospy.loginfo(pose_task)
+    # pub2Pose2D.publish(pose_task)
+    print('___msgDecode_Pose2D    end_of_message_', '\n')
 
-def callback2(data):
+
+def callback_HRIhealth(data):
     # rospy.sleep(.5)
-    print('callback___2')
-    rospy.loginfo('receiving message2222 %s', data.action)
+    print('callback_HRIhealth')
+    rospy.loginfo('receiving message2222 %s',)# data.action)
 
+    hriStateTest = HRI_HealthStatePost(address, port, 'forth.HRI.SystemHealth:001', HRI_health_jsonFName)
+    hriStateTest.updateStateMsg("OK", str(my_date.isoformat())) , '\n'
+
+def callback_ScenePHealth(data):
+    # rospy.sleep(.5)
+    print('callback_ScenePHealth')
+    rospy.loginfo('receiving message2222 %s',)# data.action)
+
+    ScenePerceptionState = HRI_HealthStatePost(address, port, 'forth.ScenePerception.SystemHealth:001',
+                                               ScenePerception_health_jsonFName)
+    ScenePerceptionState.updateStateMsg("OK", str(my_date.isoformat())), '\n'
 
 def init_receiver():
     # rospy.init_node('receiver', anonymous=True)
     rospy.loginfo('receiver_all node started')
     print('init_receiver_all always awaits.. .')
-    rospy.Subscriber('taskExec_2HRIDM', TaskExecution2HRIDM, callback1)
-    # rospy.Subscriber('HRIDM2_taskExec', HRIDM2TaskExecution, callback2)
 
+    # this for taskExecution
+    rospy.Subscriber('taskExec_2HRIDM', TaskExecution2HRIDM, callback_HRIhealth)
+
+    # this for Localization
+    rospy.Subscriber('Robot_Pose2D', Pose2D, callback_ScenePHealth)
+
+    # this Papo-test, mine personal test,
+    rospy.Subscriber('Robot_Pose2D', Pose2D, msge_decode_Pose2D)
+
+    # while
     rospy.spin()
 
 
