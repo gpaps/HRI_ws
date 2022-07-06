3922307938add28830b8e586d053f075fd833a68
mmaniada <mmaniada@ics.forth.gr>
2022-07-07 01:17:04 +0300

    Update listen_TaskExec.py

--- src/hri_dm/scripts/listen_TaskExec.py
+++ src/hri_dm/scripts/listen_TaskExec.py
@@ -22,23 +22,40 @@ navigate_state, navigate_result = 0, 0
 pickup_state, pickup_result = 0, 0
 release_state, release_result = 0, 0
 handover_state, handover_result = 0, 0
+last_toolID=-1
 
 def callback_task2exec(data):
     # navigate, grasp, releaseTool, handover
+    global navigate_state, pickup_state, release_state, handover_state, last_toolID 
+    
     rospy.loginfo('receiving message %s', data.error_type)
     if data.action == 'navigate':
         navigate_state = 1
+        pickup_state = 0
+        release_state = 0
+        handover_state = 0
 
         WorkFlowState = WorkFlowStatePost(address, port, 'forth.hri.RobotAction', workFlow_json)
         WorkFlowState.updateStateMsg_nav(data.x, data.y, data.theta, navigate_state, data.result), '\n'
 
-
     elif data.action == 'pickup':
+        navigate_state = 0
         pickup_state = 1
+        release_state = 0
+        handover_state = 0
+        last_toolID = data.tool_id
     elif data.action == 'release':
+        navigate_state = 0
+        pickup_state = 0
         release_state = 1
+        handover_state = 0
+        last_toolID = data.tool_id
     elif data.action == 'handover':
+        navigate_state = 0
+        pickup_state = 0
+        release_state = 0
         handover_state = 1
+        last_toolID = data.tool_id
 
 
 
@@ -51,6 +68,41 @@ def callback_HRIhealth(data):
     hriStateTest.updateStateMsg("OK", str(my_date.isoformat())), '\n'
     # print(data)
 
+
+    #Get information about the result of robot action execution in ROS, to be forwarded to the Orchestrator 
+def callback_TaskExResult(data):
+    global navigate_state, pickup_state, release_state, handover_state, last_toolID 
+
+    print('callback_TaskExResult')
+    rospy.loginfo('receiving message2222 %s',data)
+    
+    # if "navigate" was the active action (i.e. under execution) provide an update for this action
+    if navigate_state == 1:
+        # it has cocompleted so it is not active anymore
+        navigate_state = 0
+        navigate_result = data.result # not sure if this line is needed
+        WorkFlowState = WorkFlowStatePost(address, port, 'forth.hri.RobotAction', workFlow_json)
+        WorkFlowState.updateStateMsg_nav(data.x, data.y, data.theta, navigate_state, data.result), '\n'
+
+    # if "pickup" was the active action (i.e. under execution) provide an update for this action
+    elif pickup_state == 1:
+        # it has cocompleted so it is not active anymore
+        pickup_state = 0
+        pickup_result = data.result # not sure if this line is needed
+        WorkFlowState = WorkFlowStatePost(address, port, 'forth.hri.RobotAction', workFlow_json)
+        WorkFlowState.updateStateMsg_grasp(self, last_toolID, data.x, data.y, data.z, pickup_state,  data.result):
+
+    # if "release" was the active action (i.e. under execution) provide an update for this action
+    elif release_state == 1:
+        # it has cocompleted so it is not active anymore
+        release_state = 0 
+        release_result = data.result # not sure if this line is needed
+        WorkFlowState = WorkFlowStatePost(address, port, 'forth.hri.RobotAction', workFlow_json)
+        fingerState=0
+        armState=0
+        WorkFlowState.updateStateMsg_release(self, last_toolID, fingerState, armState,  data.result):
+            
+            
 def callback_ScenePHealth(data):
     # rospy.sleep(.5)
     print('callback_ScenePHealth')
@@ -70,7 +122,9 @@ def init_receiver():
     print('init_receiver_all always awaits.. .')
 
     # this for taskExecution
-    rospy.Subscriber('taskExec_2HRIDM', TaskExecution2HRIDM, callback_HRIhealth)
+    rospy.Subscriber('taskExec_2HRIDM', TaskExecution2HRIDM, callback_TaskExResult)
+    # rospy.Subscriber('taskExec_2HRIDM', TaskExecution2HRIDM, callback_HRIhealth)
+    
     # this for Localization
     rospy.Subscriber('Robot_Pose2D', Pose2D, callback_ScenePHealth)
     # this for FORTH/ Task2Execution
