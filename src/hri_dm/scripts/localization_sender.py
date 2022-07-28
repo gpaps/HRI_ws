1658b80b5498252d99a003e3d88e83a35355efbe
gpapo <grgepapadopoulos@gmail.com>
2022-07-28 11:51:29 +0300

    change names and values

--- src/hri_dm/scripts/localization_sender.py
+++ src/hri_dm/scripts/localization_sender.py
@@ -6,25 +6,25 @@ from datetime import datetime
 from hri_dm.msg import Pose2D
 
 # publishers
-Artificial_PublicationPose2D = rospy.Publisher('Robot_Pose2D', Pose2D, queue_size=100)
+pub2Pose2D = rospy.Publisher('Robot_Pose2D', Pose2D, queue_size=100)
 
 # callbacks
 def send_msg1_pose2d():
-    global Artificial_PublicationPose2D
+    global pub2Pose2D
     pose_task = Pose2D()
-    pose_task.x = 5.0
-    pose_task.y = 10.0
-    pose_task.theta = 60.0
+    pose_task.x = 10.0
+    pose_task.y = 20.0
+    pose_task.theta = 30.0
     my_date = datetime.utcnow()  # utc time, this is used in FELICE
     pose_task.timestamp = my_date.isoformat()
     rospy.loginfo(pose_task)
-    Artificial_PublicationPose2D.publish(pose_task)
+    pub2Pose2D.publish(pose_task)
 
 # location sender
 def location_sender():
     rospy.loginfo('artificial sender node starts...')
     send_msg1_pose2d()
-    rospy.Rate(0.5)  # t=1/f, where f = 0.5 <-- rospyRate
+    # rospy.Rate(0.5)  # t=1/f, where f = 0.5 <-- rospyRate
 
 if __name__ == '__main__':
     # init the 1st publisher  or init the first pub-in
