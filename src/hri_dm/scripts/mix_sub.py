fa27bc7d2df96fe288e3902424b974a3f2d8fa16
gpapo <grgepapadopoulos@gmail.com>
2022-09-06 14:07:05 +0300

    -Both scripts subscriptions are up2date till UNISA.SGA.Speech 1)FORTH-HRI 2)Forth_ScenePerception 3)FHOE 4)AEGIS_ButtonPress 5)UNISA.SGA.Speech

--- src/hri_dm/scripts/mix_sub.py
+++ src/hri_dm/scripts/mix_sub.py
@@ -78,17 +78,63 @@ msg_FHOE = "{" \
        "    }\n}"
 
 CB_BASE_URL = "http://{}:{}/".format(selection_address_CB, selection_port_CB)  # url send notification
-# CB_BASE_URL = "http://{}:{}/v2/".format(selection_address_CB, selection_port_CB)  # url send notification
+
+# subscription
+msg_AEGIS_ButtonPress = "{" \
+       "    \"description\": \"FORTH-U20c\",\n" \
+       "    \"subject\": {\n" \
+       "        \"entities\":\n" \
+       "        [{\n" \
+       "            \"idPattern\": \"AEGIS.Visualizations.*\",\n" \
+       "            \"typePattern\": \".*\"\n" \
+       "        }],\n" \
+       "        \"conditions\": {\n" \
+       "            \"attrs\": []\n" \
+       "        }\n" \
+       "    },\n" \
+       "    \"notification\": {\n" \
+       "        \"http\": {\n" \
+       "            \"url\": \"http://25.28.115.246:2620/\",\n" \
+       "            \"method\": \"POST\",\n" \
+       "            \"headers\": {\n" \
+       "                \"Content-Type\": \"application/json\"\n" \
+       "            }\n" \
+       "        }\n" \
+       "    }\n}"
+
+CB_BASE_URL = "http://{}:{}/v2/".format(selection_address_CB, selection_port_CB)  # url send notification
+
+# subscription
+msg_UNISA_SpeechGestureAnalysis_Speech = "{" \
+       "    \"description\": \"FORTH-U20c\",\n" \
+       "    \"subject\": {\n" \
+       "        \"entities\":\n" \
+       "        [{\n" \
+       "            \"idPattern\": \"UNISA.SpeechGestureAnalysis.*\",\n" \
+       "            \"typePattern\": \".*\"\n" \
+       "        }],\n" \
+       "        \"conditions\": {\n" \
+       "            \"attrs\": []\n" \
+       "        }\n" \
+       "    },\n" \
+       "    \"notification\": {\n" \
+       "        \"http\": {\n" \
+       "            \"url\": \"http://25.28.115.246:2620/\",\n" \
+       "            \"method\": \"POST\",\n" \
+       "            \"headers\": {\n" \
+       "                \"Content-Type\": \"application/json\"\n" \
+       "            }\n" \
+       "        }\n" \
+       "    }\n}"
+
+CB_BASE_URL = "http://{}:{}/v2/".format(selection_address_CB, selection_port_CB)  # url send notification
+
 
 # Log("INFO", "Send subcription")
 # Log("INFO", msg)
-
-#
-#
 # This is to subscribe. It is performed ONLY ONCE for each subscription.
 #
-#
-
+'\n'
 # FORTH HRI
 response = requests.post(CB_BASE_URL + "subscriptions/", data=msg_FORTH_HRI, headers=CB_HEADER)  # send request to Context Broker
 if response.ok:  # positive response, notification accepted
@@ -109,3 +155,17 @@ if response2.ok:  # positive response, notification accepted
     print("CB response1 -> status " + response2.status_code.__str__())
 else:  # error response
     print("CB response1 -> " + response2.text)
+
+# AEGIS_ButtonPress
+response3 = requests.post(CB_BASE_URL + "subscriptions/", data=msg_AEGIS_ButtonPress, headers=CB_HEADER)
+if response3.ok:  # positive response, notification accepted
+    print("CB response AEGIS_ButtonPress -> status " + response3.status_code.__str__())
+else:  # error response
+    print("CB response AEGIS_ButtonPress -> " + response3.text)
+
+# UNISA_ Speech Gesture Analysis(SGA) _Speech
+response4 = requests.post(CB_BASE_URL + "subscriptions/", data=msg_UNISA_SpeechGestureAnalysis_Speech, headers=CB_HEADER)
+if response4.ok:  # positive response, notification accepted
+    print("CB response msg_UNISA_SGA_Speech -> status " + response4.status_code.__str__())
+else:  # error response
+    print("CB response msg_UNISA_SGA_Speech -> " + response4.text)
--- src/hri_dm/scripts/sub.py
+++ src/hri_dm/scripts/sub.py
@@ -104,16 +104,39 @@ msg_AEGIS_ButtonPress = "{" \
 
 CB_BASE_URL = "http://{}:{}/v2/".format(selection_address_CB, selection_port_CB)  # url send notification
 
+# subscription
+msg_UNISA_SpeechGestureAnalysis_Speech = "{" \
+       "    \"description\": \"FORTH-RoboTest\",\n" \
+       "    \"subject\": {\n" \
+       "        \"entities\":\n" \
+       "        [{\n" \
+       "            \"idPattern\": \"UNISA.SpeechGestureAnalysis.*\",\n" \
+       "            \"typePattern\": \".*\"\n" \
+       "        }],\n" \
+       "        \"conditions\": {\n" \
+       "            \"attrs\": []\n" \
+       "        }\n" \
+       "    },\n" \
+       "    \"notification\": {\n" \
+       "        \"http\": {\n" \
+       "            \"url\": \"http://25.28.115.246:2620/\",\n" \
+       "            \"method\": \"POST\",\n" \
+       "            \"headers\": {\n" \
+       "                \"Content-Type\": \"application/json\"\n" \
+       "            }\n" \
+       "        }\n" \
+       "    }\n}"
+
+CB_BASE_URL = "http://{}:{}/v2/".format(selection_address_CB, selection_port_CB)  # url send notification
+
+
+
 
 # Log("INFO", "Send subcription")
 # Log("INFO", msg)
-
-#
-#
 # This is to subscribe. It is performed ONLY ONCE for each subscription.
 #
-#
-
+'\n'
 # FORTH HRI
 response = requests.post(CB_BASE_URL + "subscriptions/", data=msg_FORTH_HRI, headers=CB_HEADER)  # send request to Context Broker
 if response.ok:  # positive response, notification accepted
@@ -141,3 +164,10 @@ if response3.ok:  # positive response, notification accepted
     print("CB response AEGIS_ButtonPress -> status " + response3.status_code.__str__())
 else:  # error response
     print("CB response AEGIS_ButtonPress -> " + response3.text)
+
+# UNISA_ Speech Gesture Analysis(SGA) _Speech
+response4 = requests.post(CB_BASE_URL + "subscriptions/", data=msg_UNISA_SpeechGestureAnalysis_Speech, headers=CB_HEADER)
+if response4.ok:  # positive response, notification accepted
+    print("CB response msg_UNISA_SGA_Speech -> status " + response4.status_code.__str__())
+else:  # error response
+    print("CB response msg_UNISA_SGA_Speech -> " + response4.text)
