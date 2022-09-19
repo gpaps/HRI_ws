892ec3d3da321949268e5962fef0a2d23e209684
gpapo <grgepapadopoulos@gmail.com>
2022-09-19 12:29:55 +0300

    Automated procedure from connecting to vpn to start the TWO main scripts listen_TaskExec.py and listen_WFCommand.py.

--- src/hri_dm/scripts/all_scripts.sh
+++ src/hri_dm/scripts/all_scripts.sh
@@ -1,26 +1,27 @@
-sudo hamachi login 
+sudo hamachi login       # connect2vpn
 
-cd ~/Desktop/hri_dm
-source devel/setup.bash
+cd ~/Desktop/hri_dm     # path to main DIR,
+source devel/setup.bash # activate ROS environment
 
-# init ros
+# initialize ROS
 gnome-terminal -- roscore
 
+# path to Python Sripts
 cd /src/hri_dm/scripts
 
-#py listeners
-gnome-terminal -- python3 listen_TaskExec.py
-gnome-terminal -- python3 listenerSubscriber.py
-#py send once
-gnome-terminal -- python3 localSender.py
-
-# this is for conncecting/Subscribing to network, 
-# only occures once otherwise comment it out 
+# this is for connecting/Subscribing to network,
+# only occurs once otherwise comment it out
 gnome-terminal -- python3 sub.py
 echo 		Subscribed???Check above!!
 
-# fiware listen's
-python3 fiwareFORTH_cleanTest.py
+#py listener2ROS sends2Fiware
+gnome-terminal -- python3 listen_TaskExec.py
+#py listens2Fiware sends2ROS
+gnome-terminal -- python3 listenWFCommand.py
 
-# fiware sends?
-gnome-terminal -- python3 forthHRIHealthPost.py
+#py send once 3xROS topics, check py for comment's __TEST_SCRIPT__
+gnome-terminal -- python3 localSender.py
+
+
+## fiware sends?
+#gnome-terminal -- python3 forthHRIHealthPost.py
