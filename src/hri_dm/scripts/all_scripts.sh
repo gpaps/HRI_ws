71e0b1db984084d50fb3385bca78a940c09b7094
gpapo <grgepapadopoulos@gmail.com>
2022-10-31 10:48:46 +0200

    add comments

--- src/hri_dm/scripts/all_scripts.sh
+++ src/hri_dm/scripts/all_scripts.sh
@@ -1,27 +1,35 @@
-sudo hamachi login       # connect2vpn
+#### VPN OMITTED ##################################################
+#sudo hamachi login       # connect2vpn
+###################################################################
 
-cd ~/Desktop/hri_dm     # path to main DIR,
-source devel/setup.bash # activate ROS environment
-
-# initialize ROS
+#### Initialize ROS ###############################################
+cd ~/Desktop/hri_ws1    # path to main DIR,
+source ~/Desktop/hri_ws1/devel/setup.bash # activate ROS environment
 gnome-terminal -- roscore
+###################################################################
 
-# path to Python Sripts
-cd /src/hri_dm/scripts
+########## Path to Python Scripts##################################
+cd ~/Desktop/hri_ws1/src/hri_dm/scripts
 
-# this is for connecting/Subscribing to network,
-# only occurs once otherwise comment it out
-gnome-terminal -- python3 sub.py
-echo 		Subscribed???Check above!!
+################# Subscription #####################################
+# These are for Connecting/Subscribing to Network,
+# Only occurs ONCE, otherwise comment it out
+#gnome-terminal -- python3 sub.py
+#gnome-terminal -- python3 mix_sub.py
+#echo 		Subscribed???Check above!!
+####################################################################
 
 #py listener2ROS sends2Fiware
 gnome-terminal -- python3 listen_TaskExec.py
 #py listens2Fiware sends2ROS
-gnome-terminal -- python3 listenWFCommand.py
+gnome-terminal -- python3 listen_WFCommand.py m
 
+################## For Debug Script ################################
 #py send once 3xROS topics, check py for comment's __TEST_SCRIPT__
-gnome-terminal -- python3 localSender.py
+#gnome-terminal -- python3 localSender.py
+####################################################################
 
 
-## fiware sends?
-#gnome-terminal -- python3 forthHRIHealthPost.py
+################# fiware sends? ####################################
+gnome-terminal -- python3 forthHRIHealthPost.py
+####################################################################
